dd82e55b2193d7d094139dd3361e9b25c790d0df
Sidharth Soman <siddu.soman65@gmail.com>
2021-04-01 23:46:23 +0530

    added put and sort by priority

--- public/main.js
+++ public/main.js
@@ -17,7 +17,19 @@ deletes.forEach(el => {
 });
 
 completes.forEach(el => {
-    el.addEventListener('click', function(){
-        console.log('Hello World!');
+    el.addEventListener('click', async function(e){
+        const name = (e.target.parentNode.childNodes[1].innerText.split(":")[1].trim().slice(0,-1));
+        const percComp = Number(e.target.parentNode.childNodes[7].innerText.split(":")[1].trim().slice(0,-1));
+        console.log(percComp);
+        const response = await fetch('/updateComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'taskName': name,
+                'percComp': percComp
+            })
+        });
+        const data = await response.json();
+        window.location.reload();
     })
 });
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
-const urlencodedParser = bodyParser.urlencoded({extended: false});
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 require('dotenv').config();
 let db, dbName = 'todo';
 app.use(cors());
@@ -20,12 +20,13 @@ MongoClient.connect(process.env.DB_STRING, { useUnifiedTopology: true })
 
 app.get('/', (req, res) => {
     db.collection('tasks').find().toArray()
-    .then(data => {
-        res.render('index.ejs', {info: data});
-    })
-    .catch(err => {
-        console.log(err);
-    })
+        .then(data => {
+            data.sort((a,b)=>Number(b.taskpriority)-Number(a.taskpriority));
+            res.render('index.ejs', { info: data});
+        })
+        .catch(err => {
+            console.log(err);
+        })
 });
 
 app.post('/addTask', urlencodedParser, (req, res) => {
@@ -35,27 +36,47 @@ app.post('/addTask', urlencodedParser, (req, res) => {
         'taskpriority': req.body.taskPriority,
         'percentComplete': 0
     })
-    .then(data => {
-        console.log('Task has been inserted successfully');
-        res.redirect('/');
-    })
-    .catch(err => {
-        console.log(err);
-    })
+        .then(data => {
+            console.log('Task has been inserted successfully');
+            res.redirect('/');
+        })
+        .catch(err => {
+            console.log(err);
+        })
 })
 
+app.put('/updateComplete', (req, res) => {
+    console.log(req.body);
+    db.collection('tasks').updateOne({ taskName: req.body.taskName, percentComplete: req.body.percComp },
+        {
+            $set: {
+                percentComplete: req.body.percComp + 25
+            }
+        },
+        {
+            sort: { _id: -1 },
+            upsert: true
+        })
+        .then(data => {
+            res.json('Done')
+        })
+        .catch(err => {
+            console.log(err);
+        })
+});
+
 app.delete('/deleteTask', (req, res) => {
-    db.collection('tasks').deleteOne({taskName: req.body.taskName})
-    .then(data => {
-        res.json('Done');
-    })
-    .catch(err => {
-        console.log(err);
-    })
+    db.collection('tasks').deleteOne({ taskName: req.body.taskName })
+        .then(data => {
+            res.json('Done');
+        })
+        .catch(err => {
+            console.log(err);
+        })
 });
 
 app.listen(process.env.PORT, (err) => {
-    if(err) console.log(err);
+    if (err) console.log(err);
     else console.log('App running on port ' + process.env.PORT);
 });
 
